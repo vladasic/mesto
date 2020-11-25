d68a70c5593c8b671c72f54c245ef94e701b5007
Vlad Rastyagaev <vlad.rastik@yandex.ru>
2020-11-25 17:54:37 +0900

    feat:JS

---
+++ JavaScript/javaScript.js
@@ -0,0 +1,25 @@
+
+let portfileButton = document.querySelector('.portfile__button');
+let portfileSubttitle = document.querySelector('.portfile__subtitle')
+let popup = document.querySelector('.popup');
+let popupCloseIcon = popup.querySelector('.popup__close-icon');
+let inputs = document.querySelectorAll('input')
+let portfileTitle = document.querySelector('.portfile__title')
+let popupContainer = popup.querySelector('.popup__container')
+
+
+portfileButton.addEventListener('click' ,popupToggle);
+popupCloseIcon.addEventListener('click',popupToggle);
+
+
+function popupToggle(){
+    popup.classList.toggle('popup__opened')
+}
+
+function formSubmitHandler (event) {
+    event.preventDefault();
+    portfileTitle.textContent = inputs[0].value;
+    portfileSubttitle.textContent = inputs[1].value;
+}
+
+popupContainer.addEventListener('click', formSubmitHandler)

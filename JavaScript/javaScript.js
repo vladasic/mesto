b7acf63224616d8807a187411e4af32955625dee
Vlad Rastyagaev <vlad.rastik@yandex.ru>
2020-11-28 18:11:42 +0900

    fix:JavaScript blocks

--- JavaScript/javaScript.js
+++ JavaScript/javaScript.js
@@ -3,23 +3,31 @@ let portfileButton = document.querySelector('.portfile__button');
 let portfileSubttitle = document.querySelector('.portfile__subtitle')
 let popup = document.querySelector('.popup');
 let popupCloseIcon = popup.querySelector('.popup__close-icon');
-let inputs = document.querySelectorAll('input')
-let portfileTitle = document.querySelector('.portfile__title')
-let popupContainer = popup.querySelector('.popup__container')
+let inputTop = document.querySelector('.popup__item_top');
+let inputBottom = document.querySelector('.popup__item_bottom');
+let portfileTitle = document.querySelector('.portfile__title');
+let popupContainer = popup.querySelector('.popup__container');
+let popupButton = popup.querySelector('.popup__button')
 
+function popupAdd(){
+    popup.classList.add('popup_opened')
+}
 
-portfileButton.addEventListener('click' ,popupToggle);
-popupCloseIcon.addEventListener('click',popupToggle);
-
-
-function popupToggle(){
-    popup.classList.toggle('popup_opened')
+function popupRemove(){
+    popup.classList.remove('popup_opened')
 }
 
 function formSubmitHandler (event) {
     event.preventDefault();
-    portfileTitle.textContent = inputs[0].value;
-    portfileSubttitle.textContent = inputs[1].value;
+    portfileTitle.textContent = inputTop.value;
+    portfileSubttitle.textContent = inputBottom.value;
+    
+}
+function formClosed() {
+     popup.classList.remove('popup_opened')
 }
 
-popupContainer.addEventListener('click', formSubmitHandler)
+portfileButton.addEventListener('click' ,popupAdd);
+popupCloseIcon.addEventListener('click',popupRemove);
+popupContainer.addEventListener('submit', formSubmitHandler)
+popupButton.addEventListener('click', formClosed)

40f870f34ae908308d57790511e3cd6f8ea90e27
Vlad Rastyagaev <vlad.rastik@yandex.ru>
2020-12-02 12:50:00 +0900

    Fix:all

--- JavaScript/javaScript.js
+++ JavaScript/javaScript.js
@@ -1,33 +1,32 @@
-
 let portfileButton = document.querySelector('.portfile__button');
 let portfileSubttitle = document.querySelector('.portfile__subtitle')
 let popup = document.querySelector('.popup');
 let popupCloseIcon = popup.querySelector('.popup__close-icon');
-let inputTop = document.querySelector('.popup__item_top');
-let inputBottom = document.querySelector('.popup__item_bottom');
+let inputTop = document.querySelector('.popup__item_position_top');
+let inputBottom = document.querySelector('.popup__item_position_bottom');
 let portfileTitle = document.querySelector('.portfile__title');
 let popupContainer = popup.querySelector('.popup__container');
 let popupButton = popup.querySelector('.popup__button')
 
+
 function popupAdd(){
-    popup.classList.add('popup_opened')
+    popup.classList.add('popup_opened');
+    inputTop.value = portfileTitle.textContent;
+    inputBottom.value = portfileSubttitle.textContent;
+}
+function popupRemove(){
+    popup.classList.remove('popup_opened');
 }
 
-function popupRemove(){
-    popup.classList.remove('popup_opened')
-}
 
 function formSubmitHandler (event) {
     event.preventDefault();
     portfileTitle.textContent = inputTop.value;
     portfileSubttitle.textContent = inputBottom.value;
-    
+    popupButton.addEventListener('click', popupRemove);
 }
-function formClosed() {
-     popup.classList.remove('popup_opened')
-}
+
 
 portfileButton.addEventListener('click' ,popupAdd);
 popupCloseIcon.addEventListener('click',popupRemove);
-popupContainer.addEventListener('click', formSubmitHandler)
-popupButton.addEventListener('click', formClosed)
+popupContainer.addEventListener('submit', formSubmitHandler)
